9ddd801118909012c8dd8b19a1717ccd6d4dd11c
Purwadhika Fullstack <93514408+purwadhikafullstack@users.noreply.github.com>
2022-09-14 15:54:07 +0700

    Update and rename deploy-be.config.js to deploy.config.js

---
+++ deploy.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apps: [
+    {
+      name: 'JCWD-2102-04', // Format JCWD-{batchcode}-{groupnumber}
+      script: 'npm',
+      args: 'start',
+      time: true,
+    },
+  ],
+};
