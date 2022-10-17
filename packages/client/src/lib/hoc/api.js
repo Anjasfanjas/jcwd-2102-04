c398140667735a7fc90fbb3a9430be8c6a207b6b
Anjasfanjas <62926463+Anjasfanjas@users.noreply.github.com>
2022-10-17 09:26:09 +0700

    Update api.js

--- packages/client/src/lib/hoc/api.js
+++ packages/client/src/lib/hoc/api.js
@@ -3,8 +3,8 @@ import jsCookie from "js-cookie"
 import auth_types from "../../redux/reducers/types/auth"
 
 export const axiosInstance = axios.create({
-    // baseURL:"https://jcwd210204api.purwadhikabootcamp.com/"
-    baseURL:"http://localhost:2000/"
+    baseURL:"https://jcwd210204api.purwadhikabootcamp.com/"
+//     baseURL:"http://localhost:2000/"
 })
 
 axiosInstance.interceptors.request.use((config) => {
